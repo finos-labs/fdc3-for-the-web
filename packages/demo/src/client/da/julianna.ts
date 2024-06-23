a8a9fbfe000e1928ea919eb11243f8750b28ac95
Julianna Langston <julianna@cosaic.io>
2024-06-23 16:25:56 -0400

    Comments

--- packages/demo/src/client/da/julianna.ts
+++ packages/demo/src/client/da/julianna.ts
@@ -29,24 +29,13 @@ window.addEventListener("load", () => {
             sc.goodbye(id)
         });
 
+        // Fill in the slots in the DesktopAgent with requested apps
         document.querySelectorAll<HTMLDivElement>("#app-list > div").forEach((elem) => {
             const appId = elem.getAttribute("data-appId");
             if(!appId) return;
 
             sc.open(appId, elem);
         });
-
-        // let's create buttons for some apps
-        // layout.forEach(({appId, row, column, height, width}) => {
-        //     const div = document.createElement("div");
-        //     document.body.appendChild(div);
-        //     div.style.position = "absolute";
-        //     div.style.top = `${(row-1)*ROW_HEIGHT+TOP_BUMPER}px`
-        //     div.style.left = `${(column-1)*COLUMN_WIDTH}px`
-        //     div.style.height = `${(height*ROW_HEIGHT)}px`
-        //     div.style.width = `${(width*COLUMN_WIDTH)}px`
-        //     sc.open(appId, div);
-        // });
 
         // set up desktop agent handler here using FDC3 Web Loader (or whatever we call it)
         supply(sc.appChecker, sc.detailsResolver, sc.portResolver)
